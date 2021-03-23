0a74a3d885a427d85f816f8c1ab826ff0a2f1fbf
Derick Vigne <derickvigne@me.com>
2021-03-23 02:11:08 -0400

    Created Requests Migration

---
+++ database/migrations/2021_03_23_044716_create_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('filename');
+            $table->enum('stats', ['printing', 'approved', 'canceled', 'rejected', 'submitted']);
+            $table->timestamps();
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
